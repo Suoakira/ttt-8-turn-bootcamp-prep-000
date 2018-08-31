9dfb338027e1a391b6791ae1ec321c7c3fb45e3d
Stephen James <steves0089@gmail.com>
2018-08-31 09:16:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,14 +16,6 @@ def move(board, position, xo="X")
   board[position] = xo
 end
 
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
-    return true
-  end
-end
-
 def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0, 8)
     return true
